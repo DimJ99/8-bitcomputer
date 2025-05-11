1a06ede1bb82c86bd850dacce492a2b55c142703
DimJ99 <118076194+DimJ99@users.noreply.github.com>
2025-05-11 12:49:27 -0400

    alu.v

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,13 +1,13 @@
 module alu (
-  input wire enable,
-  input wire clk,
-  input wire reset,
-  input wire [2:0] op,
-  input wire [7:0] in_a,
-  input wire [7:0] in_b,
-  output wire [7:0] out,
-  output reg flag_zero,
-  output reg flag_carry
+  input  logic        enable,
+  input  logic        clk,
+  input  logic        reset,
+  input  logic [2:0]  op,
+  input  logic [7:0]  in_a,
+  input  logic [7:0]  in_b,
+  output logic [7:0]  out,
+  output logic        flag_zero,
+  output logic        flag_carry
 );
 
   // ALU operation codes
@@ -20,27 +20,38 @@ module alu (
   localparam ALU_XOR = 3'b110;
   localparam ALU_ADC = 3'b111;
 
-  reg [7:0] buff_out;
+  logic [7:0] buff_out;
 
-  always @(posedge clk or posedge reset) begin
+  always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
-      buff_out   <= 0;
-      flag_zero  <= 0;
-      flag_carry <= 0;
+      buff_out   <= 8'd0;
+      flag_zero  <= 1'b0;
+      flag_carry <= 1'b0;
     end else if (enable) begin
       case (op)
-        ALU_ADD: {flag_carry, buff_out} = in_a + in_b;
-        ALU_ADC: {flag_carry, buff_out} = in_a + in_b + flag_carry;
-        ALU_SUB: {flag_carry, buff_out} = in_a - in_b;
-        ALU_INC: {flag_carry, buff_out} = in_a + 1;
-        ALU_DEC: {flag_carry, buff_out} = in_a - 1;
-        ALU_AND: buff_out = in_a & in_b;
-        ALU_OR:  buff_out = in_a | in_b;
-        ALU_XOR: buff_out = in_a ^ in_b;
-        default: buff_out = 8'hxx;
+        ALU_ADD: {flag_carry, buff_out} <= in_a + in_b;
+        ALU_ADC: {flag_carry, buff_out} <= in_a + in_b + flag_carry;
+        ALU_SUB: {flag_carry, buff_out} <= in_a - in_b;
+        ALU_INC: {flag_carry, buff_out} <= in_a + 1;
+        ALU_DEC: {flag_carry, buff_out} <= in_a - 1;
+        ALU_AND: begin
+          buff_out   <= in_a & in_b;
+          flag_carry <= 1'b0;
+        end
+        ALU_OR: begin
+          buff_out   <= in_a | in_b;
+          flag_carry <= 1'b0;
+        end
+        ALU_XOR: begin
+          buff_out   <= in_a ^ in_b;
+          flag_carry <= 1'b0;
+        end
+        default: begin
+          buff_out   <= 8'hXX;
+          flag_carry <= 1'b0;
+        end
       endcase
-
-      flag_zero <= (buff_out == 0);
+      flag_zero <= (buff_out == 8'd0);
     end
   end
 
