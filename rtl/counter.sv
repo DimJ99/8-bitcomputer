3500f89f9b92885975e592fa8b26f3c179d91fe8
Dimitri Janeski <118076194+DimJ99@users.noreply.github.com>
2025-05-11 15:00:23 -0400

    counter.sv

---
+++ rtl/counter.sv
@@ -0,0 +1,21 @@
+module counter( 
+  input  logic              clk,
+  input  logic [7:0] in,
+  input  logic              sel_in,
+  input  logic              reset,
+  input  logic              down,
+  output logic [7:0] out
+);
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      out <= '0;
+    end else if (sel_in) begin
+      out <= in;
+    end else if (down) begin
+      out <= out - 1;
+    end else begin
+      out <= out + 1;
+    end
+  end
+
+endmodule
